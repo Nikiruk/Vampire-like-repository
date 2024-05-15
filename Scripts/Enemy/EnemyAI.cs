498ad3039ee41b47ef0c6d4b04c53e82e5459b18
Nikiruk <neodragon22@gmail.com>
2024-05-15 17:49:42 +0500

    0.016v
    EnemyMovement обновлён, теперь враги не тупят останавливаясь на месте

--- Scripts/Enemy/EnemyAI.cs
+++ Scripts/Enemy/EnemyAI.cs
@@ -1,34 +1,52 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class EnemyAI : MonoBehaviour
 {
     private float maxRaycastDistance = 100f; // Максимальная дальность raycast
     private float attackRange = 1f; // Диапазон атаки
+    private LayerMask playerLayerMask; // Слой, на котором находится игрок
 
-    public void EnemyMovement(Transform player, int moveSpeed)
+    private void Start()
     {
+        // Установите слой игрока
+        playerLayerMask = LayerMask.GetMask("Player");
+    }
+
+    public void EnemyMovement(Transform player, float moveSpeed)
+    {
+        // if (player == null)
+
         if (player != null)
         {
-
             Vector2 direction = (player.position - transform.position).normalized;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, maxRaycastDistance, 64);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, maxRaycastDistance, playerLayerMask);
+            
+            // if (hit.collider == null)
+            //     Debug.Log("HIT COLIDER IS NULL");
+
+
+            
             if (hit.collider != null && hit.collider.gameObject == player.gameObject)
             {
-                if (Vector2.Distance(transform.position, player.position) <= attackRange)
+                float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+                
+                if (distanceToPlayer <= attackRange)
                 {
                     EventManager.OnEnemy1Attack(10);
                 }
                 else
                 {
-                    transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);   
+                    transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
                 }
+            }
+            else
+            {
+                // Если Raycast не находит игрока, продолжайте движение в последнем направлении к игроку
+                transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
             }
         }
     }
 }
+
 
 
--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 
@@ -5,13 +6,37 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // private Transform playerTransform;
+
+    // void Start()
+    // {
+        // playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        // StartCoroutine(Spawn());
+        // EventManager.Enemy1Died += OnEnemyDied;
+    // }
+
     void Update()
     {
         MapGenerate();
+        // EnemySpawning();
     }
 
     protected virtual void MapGenerate()
     {
-        //Генерация карты в потомке GridSetup
+        //Генерация карты из потомка GridSetup
     }
+
+    // protected virtual void EnemySpawning()
+    // {
+    //     //Генерация врагов из потомка EnemySpawn
+    // }
+
+    // private IEnumerator Spawn()
+    // {
+    //     while (true)
+    //     {
+    //         EnemySpawning();
+    //         yield return new WaitForSeconds(1f); // Ожидание 1 секунды
+    //     }
+    // } 
 }
